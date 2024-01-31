cd171c9afc69152091530333fce570151b440c44
Mohamed Abu Baker <mohamedabubaker09@gmail.com>
2024-01-31 13:53:22 +0200

    Fixing bugs

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ class Bot:
 
         self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/',
                                              timeout=60,
-                                             certificate=open("lanabot_public_crt.pem", 'r'))
+                                             certificate=open("lanabot_public.pem", 'r'))
 
     def send_text(self, chat_id, text, reply_markup=None):
         message = self.telegram_bot_client.send_message(chat_id, text, reply_markup=reply_markup)
